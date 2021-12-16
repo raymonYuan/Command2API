5d0d903882898178f75e34848a55d7d87b64e393
raymonYuan <1131510575@qq.com>
2021-12-16 17:33:39 +0800

    Add files via upload

--- Command2Api.py
+++ Command2Api.py
@@ -1,15 +1,16 @@
 import subprocess
-import BaseHTTPServer
-import SimpleHTTPServer
+# import BaseHTTPServer
+# import SimpleHTTPServer
 import cgi
 import threading
 import sys
 import string
 import random
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 l = []
-
-uri = '/' + ''.join(random.sample(string.ascii_letters+string.digits,8))
+uri = "/getRecord"
+checkuri = "/checkAlive"
 
 class thread(threading.Thread):
 	def __init__(self, threadname, command):
@@ -27,26 +28,31 @@ class thread(threading.Thread):
 			stderr=subprocess.PIPE
 		)
 		for i in iter(ret.stdout.readline, b""):
-			res = i.decode().strip()
-			print(res)
-			l.append(res)
+			l.append(i.decode().strip())
+			print(l)
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class ServerHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		global l
 		if self.path == uri:
+			lstr = "\n".join(l)
 			self.send_response(200)
-			self.send_header('Content-Type', 'text/plain')
+			self.send_header('Content-type', 'application/json')
 			self.end_headers()
-			self.wfile.write(l)
+			self.wfile.write(lstr.encode())
+		if self.path == checkuri:
+			self.send_response(200)
+			self.send_header('Content-type', 'application/json')
+			self.end_headers()
+			msg = "commandAPI works!"
+			self.wfile.write(msg.encode())
 
 if __name__ == '__main__':
-	# New Thread: Get Command Result
+	print("Usage:python3 Command2Api.py yourCommand APIPort")
 	t1 = thread('1', sys.argv[1])
 	t1.start()
-	# Webserver
 	port = int(sys.argv[2])
-	print("URL: http://HOST:{0}{1}".format(port, uri))
+	print("URL: http://{0}:{1}{2}".format("127.0.0.1",port, uri))
 	Handler = ServerHandler
-	httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', port), Handler)
+	httpd = HTTPServer(('0.0.0.0', port), Handler)
 	httpd.serve_forever()
